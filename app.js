d9c5e68ddf8e9bd0e931c5ee4b7bb24775258270
Ajay Dubey <2beyajay@gmail.com>
2020-07-15 01:33:06 -0400

    take user input with POST and write it in a file

--- app.js
+++ app.js
@@ -16,8 +16,19 @@ const server = http.createServer((req, res) => {
 	// not we have to control what happens when they get to /message
 
 	if (url === "/message" && method === "POST") {
-		fs.writeFileSync('message.txt', 'DUMMY TEXT') // creating a file when the method is post and the url is /message
+		const body = [];
+		
+		req.on('data', (chunk) => {
+			console.log(chunk);
+			body.push(chunk);
+		}); //data comes from POST in chunks. so we're collecting it all in body
 
+		req.on('end', () => {
+			const parseBody = Buffer.concat(body).toString(); // converting array to string
+			const message = parseBody.split('=')[1]; // the data comes in key = value pair. taking out the key and saving the value 
+			fs.writeFileSync('message.txt', message);
+		})
+		
 		res.writeHead(302, {location: '/'}) //302 = response code for redirect. then location is to declare where you want it to redirect
 		return res.end();
 	}
