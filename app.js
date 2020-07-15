e9aae9ccf20db778612e6d7c264710a16fd93950
Ajay Dubey <2beyajay@gmail.com>
2020-07-14 21:21:53 -0400

    Handling redirect after form post

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
-const http = require('http');
+const http = require("http");
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+	const url = req.url;
+	const method = req.method;
 
-    res.setHeader('Content-Type', 'text/html'); //telling the client what kind of response we are sending to it
-    
-    res.write('<html><body>hello world</body>') //the html response we send back to the client(browser)
+	if (url === "/") {
+		res.write(
+			'<html><body><form action="/message" method="POST"><input type="text" name="message"></input><button>Send</button></form></body>'
+		);
+		return res.end();
+	}
 
-    res.end(); //ending the response. the client will stop loading
+	// on clicking the submit button, the user will be redirected to /message due to the action of the form.
+	// not we have to control what happens when they get to /message
 
+	if (url === "/message" && method === "POST") {
+		fs.writeFileSync('message.txt', 'DUMMY TEXT') // creating a file when the method is post and the url is /message
+
+		res.writeHead(302, {location: '/'}) //302 = response code for redirect. then location is to declare where you want it to redirect
+		return res.end();
+	}
+
+
+	res.setHeader("Content-Type", "text/html");
+	res.write("<html><body>hello world</body>");
+	res.end();
 });
 
-server.listen(3000); // keep listening for requests on port 3000
+server.listen(3000); 
