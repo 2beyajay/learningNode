9263dd172ea7ba1659758a66bcc0c058d8bb4bbd
Ajay Dubey <2beyajay@gmail.com>
2020-07-18 19:23:10 -0400

    initialized .gitignore file for node_modules

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const http = require("http");
-const routes = require('./routes')
+const http = require('http');
+
+const routes = require('./routes');
 
 console.log(routes.someText);
 
-const server = http.createServer(routes.handler); //exported object {routes} from the other file
+const server = http.createServer(routes.handler);
 
-server.listen(3000); 
+server.listen(3000);
--- routes.js
+++ routes.js
@@ -1,54 +1,50 @@
-const fs = require('fs')
+const fs = require('fs');
 
 const requestHandler = (req, res) => {
-    const url = req.url;
-    const method = req.method;
-
-    if (url === "/") {
-        res.write(
-            '<html><body><form action="/message" method="POST"><input type="text" name="message"></input><button>Send</button></form></body>'
-        );
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write('</html>');
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', chunk => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFile('message.txt', message, err => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
         return res.end();
-    }
+      });
+    });
+  }
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title><head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  res.write('</html>');
+  res.end();
+};
 
-    // on clicking the submit button, the user will be redirected to /message due to the action of the form.
-    // not we have to control what happens when they get to /message
-
-    if (url === "/message" && method === "POST") {
-        const body = [];
-
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        }); //data comes from POST in chunks. so we're collecting it all in body
-
-        req.on('end', () => {
-            const parseBody = Buffer.concat(body).toString(); // converting array to string
-            const message = parseBody.split('=')[1]; // the data comes in key = value pair. taking out the key and saving the value 
-            fs.writeFile('message.txt', message, (err) => {
-                res.writeHead(302, {
-                    location: '/'
-                }) //302 = response code for redirect. then location is to declare where you want it to redirect
-                console.log("ended");
-                return res.end();
-            });
-        });
-
-    }
-
-
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html><body>hello world</body>");
-    res.end();
-}
-
-// module.exports = requestHandler; // export 1 thing
+// module.exports = requestHandler;
 
 // module.exports = {
 //     handler: requestHandler,
-//     someText: "some hard coded text"
-// }; //export multiple things as an object
+//     someText: 'Some hard coded text'
+// };
+
+// module.exports.handler = requestHandler;
+// module.exports.someText = 'Some text';
 
 exports.handler = requestHandler;
-exports.someText = "some hard coded text";
-// this is a shortcut. get rid of "module" and node will understand
+exports.someText = 'Some hard coded text';
