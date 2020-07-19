9140c4feae721e078695067dddbb823972c1690f
Ajay Dubey <2beyajay@gmail.com>
2020-07-18 22:59:43 -0400

    middleware "use" and sending response

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-const server = http.createServer(app);
+// "use" is a function from express that takes another function as a parameter
 
-server.listen(3000);
+app.use((req, res, next) => { //this is a middleware. part of the express.
+	console.log('in the middleware!');
+	next(); // sends the control to the next use() function. the server will be stopped here if no "next" function is used.
+});
+
+app.use((req, res, next) => {
+	console.log('and another!');
+	res.send('<h1>Hello Express</h1>');
+});
+
+app.listen(3000); // app can listen so we don't have to import the http
