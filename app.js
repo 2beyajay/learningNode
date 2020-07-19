130163611fe68bca1c08301e9c1a59561f706d4c
Ajay Dubey <2beyajay@gmail.com>
2020-07-19 02:24:11 -0400

    sending html file responses. getting project root path.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -8,13 +9,13 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 
 // 404
 app.use((req,res,next) => {
-	res.status(404).send('<h1>404 Page not found</h1>');
+	res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,16 @@
+const path = require('path');
 const express = require('express')
-
 const router = express.Router();
 
+const rootDir = require('../util/path');
+
 router.get('/add-product', (req, res) => {
-	res.send('<form action="/product" method="POST"><input type"text" name="title"/><button>Add Product</button></form>');
+	res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 });
 
-router.post('/product', (req, res) => {
+router.post('/add-product', (req, res) => {
 	console.log(req.body);
 	res.redirect('/');
 })
-
 
 module.exports = router;
