fae816b30805ca77a50577f56ee70b8552ae2624
Ajay Dubey <2beyajay@gmail.com>
2020-07-25 21:49:49 -0400

    expanding and reorganizing the project

--- routes/admin.js
+++ routes/admin.js
@@ -2,12 +2,17 @@ const path = require('path');
 const express = require('express')
 const router = express.Router();
 
-const productsController = require('../controllers/products')
+const adminController = require('../controllers/admin')
 
 
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
 
-router.get('/add-product', productsController.getAddProduct);
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
 
-router.post('/add-product', productsController.postAddProduct)
+// admin/products => GET
+router.get('/products', adminController.getProducts)
+
 
 module.exports = router
